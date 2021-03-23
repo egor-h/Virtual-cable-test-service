443850840b99cd16631764c409a52068eeca0966
Yegor H <someone@somewhere>
2021-03-23 09:45:16 +0500

    add new groups page, use new zabbix rest service instead of database queries, refactor templates

--- src/main/java/com/application/controller/MainController.java
+++ src/main/java/com/application/controller/MainController.java
@@ -4,6 +4,7 @@ import com.application.model.Host;
 import com.application.model.IfTableRow;
 import com.application.service.HostService;
 import com.application.service.VirtualCableTestService;
+import com.application.service.zabbix.ZabbixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,49 +12,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 public class MainController {
-
-    private HostService hostService;
     private VirtualCableTestService vct;
+    private ZabbixService zabbix;
 
-    @Autowired
-    public MainController(HostService hostService, VirtualCableTestService vct) {
-        this.hostService = hostService;
+    public MainController(VirtualCableTestService vct, ZabbixService zabbix) {
         this.vct = vct;
+        this.zabbix = zabbix;
     }
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("hosts", hostService.allHosts());
+        model.addAttribute("groups", zabbix.groups().getGroups());
+        return "allGroups";
+    }
+
+    @RequestMapping("/hosts")
+    public String hosts(Model model, @RequestParam(name = "grp", required = false) Optional<String> groupId) {
+        model.addAttribute("hosts", groupId.map(zabbix::hosts).orElseGet(zabbix::hosts).getResult());
         return "allHosts";
     }
 
     @RequestMapping("/vct")
-    public String vct(Model model, @RequestParam(name = "ip", required = false) String ip,
-                      @RequestParam(name = "hostid", required = false) long hostid,
+    public String vct(Model model, @RequestParam(name = "ip", required = true) String ip,
                       @RequestParam(name = "port") int port) {
 
-        if (ip == null && hostid == 0) {
-            throw new RuntimeException("No host selected");
-        }
-
-        Host host = hostService.host(hostid);
-        vct.startVCT(host, port);
-        model.addAttribute("vctResult", vct.getVCT(host, port));
+//        Host host = hostService.host(hostid);
+        vct.startVCT(ip, port);
+        model.addAttribute("vctResult", vct.getVCT(ip, port));
 
         return "vctResult";
     }
 
     @RequestMapping("/ports")
-    public String ports(Model model, @RequestParam("hostid") long hostid) {
-        Host host = hostService.host(hostid);
-        Collection<IfTableRow> ifTable = vct.getIfTable(host);
-
-        model.addAttribute("host", host);
+    public String ports(Model model, @RequestParam("ip") String ip) {
+        Collection<IfTableRow> ifTable = vct.getIfTable(ip);
+        model.addAttribute("ip", ip);
         model.addAttribute("iftable", ifTable);
-
         return "ports";
     }
 

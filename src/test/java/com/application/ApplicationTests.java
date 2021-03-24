47f2cfd58f9dcf38e248bc100215d6e2fa23a7cf
Yegor H <someone@somewhere>
2021-03-24 12:28:16 +0500

    add initial test

---
+++ src/test/java/com/application/ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.application;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
